03c5d86896216127af0fdc2ba9c5088ab0bfb1b8
ericberglund <ericsamuelberglund@gmail.com>
2015-11-10 22:51:20 -0600

    Updated Post View, Added some input validation

--- www/js/controllers/postController.js
+++ www/js/controllers/postController.js
@@ -2,4 +2,7 @@ angular.module('controllers.post', [])
 
 .controller('PostCtrl', function($scope, $ionicHistory) {
 
+    $scope.postTicket = function(){
+        console.log("Ticket Has Been Posted")
+    }
 });
