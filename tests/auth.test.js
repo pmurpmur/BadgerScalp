f58b4dd75cf92903c0c8d41b3fa6d2916e5cf1c5
Patrick Murray <pmurpmur@gmail.com>
2015-11-16 21:11:03 -0600

    test test test
    some passing now

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -24,96 +24,121 @@ describe('During authorization', function() {
         var password = element(by.model('user.password'));
 
         // send input to respective form fields
-        username.sendKeys(newEmail);
+        username.sendKeys('unlikelyemailtohave@bs.io');
         password.sendKeys('badgerscalp');
 
         // click the login button
         var loginButton = element(by.id('loginButton'));
         loginButton.click().then(function() {
-            expect(browser.getLocationAbsUrl()).toMatch('/register');
+            setTimeout(function() {
+                expect(browser.getLocationAbsUrl()).toMatch('/register');
 
-            var popup = element(by.css('.popup-container.popup-showing.active'));
-            expect(popup.isDisplayed()).toBeTruthy();
+                var popup = element(by.css('.popup-container.popup-showing.active'));
+                expect(popup.isDisplayed()).toBeTruthy();
+            }, 2000);
         });
     })
 
-    // it('should signup, logout, and login with newly created account', function() {
-    //     register.click();
+    it('should signup, logout, and login with newly created account', function() {
+        register.click();
 
-    //     var pass = 'badgerscalp';
+        var pass = 'badgerscalp';
 
-    //     var firstName = element(by.model('user.firstName'));
-    //     var lastName = element(by.model('user.lastName'));
-    //     var username = element(by.model('user.email'));
-    //     var password = element(by.model('user.password'));
+        var firstName = element(by.model('user.firstName'));
+        var lastName = element(by.model('user.lastName'));
+        var username = element(by.model('user.email'));
+        var password = element(by.model('user.password'));
 
-    //     firstName.sendKeys('Buckingham');
-    //     lastName.sendKeys('Badger');
-    //     username.sendKeys(newEmail);
-    //     password.sendKeys(pass);
+        firstName.sendKeys('Buckingham');
+        lastName.sendKeys('Badger');
+        username.sendKeys(newEmail);
+        password.sendKeys(pass);
 
-    //     var registerButton = element(by.id('registerButton'));
-    //     registerButton.click().then(function() {
-    //         expect(browser.getLocationAbsUrl()).toMatch('/browse');
-    //     });
+        var registerButton = element(by.id('registerButton'));
+        registerButton.click().then(function() {
+            setTimeout(function() {
+                expect(browser.getLocationAbsUrl()).toMatch('/browse');
+            }, 2000);
+        });
 
-    //     var registerButton = element(by.id('menuButton'));
-    //     menuButton.click().then(function() {
-    //         var logoutButton = element(by.id('logButton'));
-    //         logoutButton.click().then(function() {
-    //             expect(browser.getLocationAbsUrl()).toMatch('/login');
-    //         })
-    //     });
+        var registerButton = element(by.id('menuButton'));
+        menuButton.click().then(function() {
+            setTimeout(function() {
+                var logoutButton = element(by.id('logButton'));
+                logoutButton.click().then(function() {
+                    expect(browser.getLocationAbsUrl()).toMatch('/login');
+                })
+            }, 2000);
+        });
 
-    //     var username = element(by.model('user.email'));
-    //     var password = element(by.model('user.password'));
+        var username = element(by.model('user.email'));
+        var password = element(by.model('user.password'));
 
-    //     username.sendKeys(newEmail);
-    //     password.sendKeys(pass);
+        username.sendKeys(newEmail);
+        password.sendKeys(pass);
 
-    //     var loginButton = element(by.id('loginButton'));
-    //     loginButton.click().then(function() {
-    //         expect(browser.getLocationAbsUrl()).toMatch('/register');
+        var loginButton = element(by.id('loginButton'));
+        loginButton.click().then(function() {
+            setTimeout(function() {
+                expect(browser.getLocationAbsUrl()).toMatch('/register');
 
-    //         var popup = element(by.css('.popup-container.popup-showing.active'));
-    //         expect(popup.isDisplayed()).toBeTruthy();
-    //     });
-    // })
+                var popup = element(by.css('.popup-container.popup-showing.active'));
+                expect(popup.isDisplayed()).toBeTruthy();
+            }, 2000);
+        });
+    })
 
-    // it('should initialize the local storage with current user info', function() {
-    //     var gl = element(by.id('googleButton'));
-    //     gl.click().then(function() {
-    //         expect(browser.getLocationAbsUrl()).toMatch('/browse');
+    it('should initialize the local storage with current user info', function() {
+        var username = element(by.model('user.email'));
+        var password = element(by.model('user.password'));
 
-    //         var user = JSON.parse(localStorage.getItem('user'));
-    //         expect(user).toBeDefined();
-    //     });
-    // })
+        username.sendKeys('register@imp.io');
+        password.sendKeys('register');
 
-    // it('should signup/login via google', function() {
-    //     var gl = element(by.id('googleButton'));
-    //     gl.click().then(function() {
-    //         expect(browser.getLocationAbsUrl()).toMatch('/browse');
-    //     });
-    // })
+        var loginButton = element(by.id('loginButton'));
+        loginButton.click().then(function() {
+            setTimeout(function() {
+                expect(browser.getLocationAbsUrl()).toMatch('/browse');
 
-    // it('should signup/login via google', function() {
-    //     var fb = element(by.id('facebookButton'));
-    //     fb.click().then(function() {
-    //         expect(browser.getLocationAbsUrl()).toMatch('/browse');
-    //     });
-    // })
+                var user = JSON.parse(localStorage.getItem('user'));
+                console.log(user);
+                expect(user).toBeDefined();
+            }, 2000);
+        });
+    })
 
-    // it('should display error if invalid email is entered into forgot password field', function() {
-    //     forgotPassword.click();
+    it('should signup/login via google', function() {
+        var gl = element(by.id('googleButton'));
+        gl.click().then(function() {
+            setTimeout(function() {
+                expect(browser.getLocationAbsUrl()).toMatch('/browse');
+            }, 3000);
+        });
+    })
 
-    //     var email = element(by.model('user.email'));
-    //     email.sendKeys('unlikelyemailtohave@imp.io');
+    it('should signup/login via facebook', function() {
+        var fb = element(by.id('facebookButton'));
+        fb.click().then(function() {
+            setTimeout(function() {
+                expect(browser.getLocationAbsUrl()).toMatch('/browse');
+            }, 3000);
+        });
+    })
 
-    //     var retrieveButton = element(by.id('retrieveButton'));
-    //     retrieveButton.click().then(function() {
-    //         var popup = element(by.css('.popup-container.popup-showing.active'));
-    //         expect(popup.isDisplayed()).toBeTruthy();
-    //     });
-    // });
+    it('should display error if invalid email is entered into forgot password field', function() {
+        forgotPassword.click().then(function() {
+            setTimeout(function() {
+                var email = element(by.model('user.email'));
+                email.sendKeys('unlikelyemailtohaveimp.io');
+
+                var retrieveButton = element(by.id('retrieveButton'));
+                retrieveButton.click().then(function() {
+                    setTimeout(function() {
+                        var popup = element(by.css('.popup-container.popup-showing.active'));
+                        expect(popup.isDisplayed()).toBeTruthy();
+                    }, 2000);
+                });
+            }, 2000);
+        });
+    });
 });
