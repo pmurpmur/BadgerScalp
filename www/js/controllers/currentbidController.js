ad96517817c84592c63824eeb6604f26b17909aa
biboamy <biboamybibo@gmail.com>
2015-11-24 14:48:14 -0500

    current post and bid edit

--- www/js/controllers/currentbidController.js
+++ www/js/controllers/currentbidController.js
@@ -3,8 +3,12 @@ angular.module('controllers.currentbid', [])
 .controller('CurrentBidCtrl', function($scope, DBManager, UserStorage) {
 	$scope.device = window.screen.width;
 	
+
 	$scope.tickets = DBManager.getAllListings();
+	$scope.bids = DBManager.getAllBids();
 	$scope.userId = UserStorage.getUserId();
+
+	
 
 
 });
