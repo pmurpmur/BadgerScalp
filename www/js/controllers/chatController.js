6c0634c94735963418805411624735393082ff3b
Ingkarat <ingkarat_bew@hotmail.com>
2015-12-02 08:43:22 -0600

    Change middle tab to "browse" like interface
    - Create dummy tickets for middle tab.
    - Move local notification to localnoti.html.

--- www/js/controllers/chatController.js
+++ www/js/controllers/chatController.js
@@ -1,30 +1,71 @@
 angular.module('controllers.chat', [])
 
-.controller('ChatCtrl', function($scope,$cordovaLocalNotification,$ionicPlatform) {
+.controller('ChatCtrl', function($scope) {
 
-	$scope.noti = {};
-	$scope.noti.ntitle = "";
-	$scope.noti.ntext = "";
+	$scope.tickets = [
+		{
+			$id: 1,
+			$priority: null,
+			createdAt: 1234567,
+			date: "Wed Dec 02 2015 00:00:00 GMT-0600 (CST)",
+			details: "",
+			image: "",
+			price: 10,
+			quantity: 1,
+			seller: "google:116689779478426229041",
+			title: "Ticket 1",
+			type: "Soccer",
+			updatedAt: 1448941013906
+		},{
+			$id: 2,
+			$priority: null,
+			createdAt: 1234567,
+			date: "Wed Dec 02 2015 00:00:00 GMT-0600 (CST)",
+			details: "",
+			image: "",
+			price: 30,
+			quantity: 1,
+			seller: "google:116689779478426229041",
+			title: "Ticket 2",
+			type: "Football",
+			updatedAt: 1448941013906
+		},{
+			$id: 3,
+			$priority: null,
+			createdAt: 1234567,
+			date: "Wed Dec 02 2015 00:00:00 GMT-0600 (CST)",
+			details: "",
+			image: "",
+			price: 77,
+			quantity: 1,
+			seller: "google:116689779478426229041",
+			title: "Ticket 3",
+			type: "Music",
+			updatedAt: 1448941013906
+		},{
+			$id: 4,
+			$priority: null,
+			createdAt: 1234567,
+			date: "Wed Dec 02 2015 00:00:00 GMT-0600 (CST)",
+			details: "",
+			image: "",
+			price: 40,
+			quantity: 2,
+			seller: "google:116689779478426229041",
+			title: "Ticket 4",
+			type: "Tennis",
+			updatedAt: 1448941013906
+		},
 
-	$scope.pushNoti = function(){
+	];
 
-		$ionicPlatform.ready(function() {
-			$cordovaLocalNotification.schedule({
-				id: 1,
-				title: $scope.noti.ntitle,
-				text: $scope.noti.ntext,
-				sound: null,
-				data: { customProperty: 'custom value' }
-			});
-		});
-
-
-	}
-
-	$scope.clearNoti = function(){
-		$scope.noti.ntitle = "";
-		$scope.noti.ntext = "";		
-	}
+	$scope.localDate = function(date) {
+		if (date === undefined) {
+			return 'n/a';
+		} else {
+			return (new Date(date)).toLocaleDateString();
+		}
+	}	
 
 
 
